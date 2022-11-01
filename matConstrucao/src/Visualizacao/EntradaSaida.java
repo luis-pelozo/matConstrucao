98b14fa9e37b529ed2a5c1e6638848283b34cb41
luis-pelozo <pelozoluis52@gmail.com>
2022-10-31 21:58:02 -0300

    Iinicio do EntradaSaida

--- matConstrucao/src/Visualizacao/EntradaSaida.java
+++ matConstrucao/src/Visualizacao/EntradaSaida.java
@@ -1,5 +1,17 @@
 package Visualizacao;
 
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+
 public class EntradaSaida {
+
+    public static int solicitaOpcao() {
+		String[] opcoes = { "Cadastrar", "Mostrar lista produtos", "Adicionar produtos", "Vender",
+				"Estoque de produtos", "Cupons impressos", "Valor total de cupons", "Sair da loja" };
+
+		JComboBox<String> menu = new JComboBox<String>(opcoes);
+		JOptionPane.showConfirmDialog(null, menu, "Selecione a ação desejada", JOptionPane.OK_CANCEL_OPTION);
+		return menu.getSelectedIndex();
+	}
     
 }
